50ce2aaa876e94e0d5edf1bf192322e0f9f0fb42
tomasz <tomasznicieja@gmail.com>
2016-08-10 21:01:13 +0200

    basic structure of angular app

---
+++ js/app.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Nicieja Tomasz on 10.08.2016.
+ */
+
+var ntApp = angular.module('ntApp', []);
---
+++ js/controllers/homepage.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Nicieja Tomasz on 10.08.2016.
+ */
+
+ntApp.controller('HomepageController', function ($scope) {
+
+    $scope.init = function () {
+        console.log('Homepage :: init');
+    };
+
+});
